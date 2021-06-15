a7fc880ae7d26f90065ac357f841160e5a960a9e
Femi Ayo-Tubosun <femiaytubosun@gmail.com>
2021-06-15 22:51:25 +0100

    Adding comments

--- app/hostel/views.py
+++ app/hostel/views.py
@@ -30,22 +30,31 @@ def hostel(id):
         flash('Please fill traits before picking your room', 'error')
         return redirect(url_for('user_bp.traits'))
 
+    # Get the hostel
     hostel = get_one_query(Hostel, id)
 
+    # Check if the gender can be assigned to that hostel
     if not((hostel.hosteltype == "Co-Ed") or (hostel.hosteltype == current_user.gender)):
         flash('You cannot be assigned to this hostel', 'error')
         return redirect(url_for('hostel_bp.hostels'))
 
-    # Check for empty rooms and occupied rooms
+    # Create an empty list for empty rooms and occupied rooms
     empty_rooms = []
     occupied_rooms = []
 
+    # hostel.rooms is a list of rooms in that hostel. Loop through the rooms.
     for room in hostel.rooms:
+        # For each room, there are occupants, which is a list of users in that room
+
+        # Check if the room is empty, add to empty rooms if it is.
         if len(room.occupants) == 0:
             empty_rooms.append(room)
             continue
+
+        # Else add to occupied rooms.
         occupied_rooms.append(room)
-    # Get Match for Rooms
+
+    # Do match for occupied rooms.
     for room in occupied_rooms:
         accum = 0
         room_mem = 0
